ce108a51b06c69e1a9b63e25658e40338008770f
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-01-25 13:46:08 -0500

    linking the category and product model

--- models/index.js
+++ models/index.js
@@ -1,3 +1,11 @@
 const Category = require('../models/Category');
 const Product = require('./Product');
 const ProductTag = require('../models/productTag');
+
+
+// linking the Category and Product model
+Product.belongsTo(Category, {
+    foreignkey: 'category_id'
+})
+
+Category.hasMany(Product, { foreignKey: 'category_id' });
