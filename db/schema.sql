fba64861c8525f80e6c42146cea63558a12cede6
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-01-26 20:23:30 -0500

    created login route not complete yet

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,6 @@
+-- - delete database if cars inventory database exists
 DROP DATABASE IF EXISTS  cars_inventory_db;
 
+
+-- Create new Database
 CREATE DATABASE  cars_inventory_db;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
 const router = require('express').Router();
+const { UPSERT } = require('sequelize/dist/lib/query-types');
 const apiRoutes = require('./api');
-
+const { Users } = require('./users');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+    res.send("<h1>Wrong Route!</h1>")
 });
+
+
+
+app.post('/login/:username&:email&:password', (req, res) => {
+    Users.create({
+        username: req.params.username
+            // placeholder for username
+            ,
+        email: req.params.email,
+        password: req.params.password
+            // placeholder for password
+    })
+
+
+})
+
+app.listen(process.env || PORT, () => { "server started on port" + PORT });
 
 module.exports = router;
