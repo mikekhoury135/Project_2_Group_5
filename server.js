e83e1619581863860e5745f7a3a8d8f33efb3b50
Michael Khoury <mikekhoury135@gmail.com>
2022-01-29 10:50:01 -0500

    updated server js

--- server.js
+++ server.js
@@ -1,12 +1,45 @@
+// added more requirements based on what we might need
+
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// sequelize config
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+// app.use(session(sess));
+
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(require('./controllers/'));
 
 // turn on routes
 app.use(routes);
@@ -15,6 +48,7 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-  // app.listen(PORT, () => {
+
+// app.listen(PORT, () => {
 //     console.log(`App listening on port ${PORT}!`);
 //   });
