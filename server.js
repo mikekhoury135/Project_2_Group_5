25eb003b4de66791158b4db98e8055a4b4c06262
perry.davis242@protonmail.com <perry.davis242@protonmail.com>
2022-01-29 10:43:29 -0500

    started handlebars for user log in

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
@@ -7,14 +8,22 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log('Now listening'));
 });
-  // app.listen(PORT, () => {
+// app.listen(PORT, () => {
 //     console.log(`App listening on port ${PORT}!`);
 //   });
+
+// variables for log in handbars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
