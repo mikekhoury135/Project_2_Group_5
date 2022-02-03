678128c5c6a402a65bc999c86ae3aa7fa58521af
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-02-02 22:17:03 -0500

    added home view login view and login fetch post method script

--- controllers/api/home-routes.js
+++ controllers/api/home-routes.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    res.render('home');
 });
+
+
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+
 
 module.exports = router;
