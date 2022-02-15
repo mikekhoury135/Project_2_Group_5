16cf49e05aa8ed9e9dc6c81d296467becd4a7054
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-02-14 22:31:57 -0500

    version 1

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,19 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 
-const sequelize = process.env.JAWSDB_URL ?
-    new Sequelize(process.env.JAWSDB_URL) :
-    new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
-        logging: true,
-        dialectOptions: {
-            decimalNumbers: true,
-        },
+        port: 3306
     });
+}
+
+
 
 
 
