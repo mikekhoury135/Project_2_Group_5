d7eb3944c7f5c43f7d96bd558e372987ce07a071
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-02-23 10:09:05 -0500

    added conversion of images to blobs

--- utils/helpers.js
+++ utils/helpers.js
@@ -10,8 +10,57 @@ module.exports = {
 
 
 
-    getImg1: {
-        fetch("/images/1.jpeg").then(response => {
+    getImgAccord: {
+        fetch("/images/accord.png").then(response => {
+            response.blob();
+        }).then(blob => {
+            return FirstImage = blob;
+        })
+    }
+    getImgM5: {
+        fetch("/images/bmw 5.jpeg").then(response => {
+            response.blob();
+        }).then(blob => {
+            return FirstImage = blob;
+        })
+    }
+    getImgM3: {
+        fetch("/images/bmw3.jpeg").then(response => {
+            response.blob();
+        }).then(blob => {
+            return FirstImage = blob;
+        })
+    }
+    getImgCivic: {
+        fetch("/images/civic.png").then(response => {
+            response.blob();
+        }).then(blob => {
+            return FirstImage = blob;
+        })
+    }
+    getImgCorolla: {
+        fetch("/images/cor.png").then(response => {
+            response.blob();
+        }).then(blob => {
+            return FirstImage = blob;
+        })
+    }
+    getImgF150: {
+        fetch("/images/f150.png").then(response => {
+            response.blob();
+        }).then(blob => {
+            return FirstImage = blob;
+        })
+    }
+    getImgSupra: {
+        fetch("/images/sup.png").then(response => {
+            response.blob();
+        }).then(blob => {
+            return FirstImage = blob;
+        })
+    }
+    getImgWindstar: {
+        fetch("/images/Windstar.png").then(response => {
             response.blob();
         }).then(blob => {
             return FirstImage = blob;
